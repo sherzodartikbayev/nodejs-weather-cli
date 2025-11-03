4d23c142e9f6d6c5f34612c2c92e77c3d32042f1
Sherzod Artikbayev <info@web-pro.uz>
2025-11-03 23:55:47 +0500

    feat: Process.argv

---
+++ index.js
@@ -0,0 +1,22 @@
+const getArgs = require('./helpers/args')
+
+const startCLI = () => {
+	const args = getArgs(process.argv)
+	console.log(args)
+
+	if (args.h) {
+		// help
+	}
+
+	if (args.s) {
+		// save city
+	}
+
+	if (args.t) {
+		// save token
+	}
+
+	// result
+}
+
+startCLI()
